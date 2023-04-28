5e635a36f0a3682bcb2da2ae12c7c2fdaad7895b
Rahul Mahato <103503943+rahul232010@users.noreply.github.com>
2023-04-29 01:31:27 +0530

    Add files via upload
    LGMVP CALCULATOR

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import CalHead from './Project2/CalHead';
+import CalcuLator from './Project2/CalcuLator';
+
+
+const App =() => {
+  return(
+    <>
+      <CalHead />
+      <CalcuLator />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Project2/CalHead.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CalHead =() => {
+    return(
+        <div className="head">
+        <div className="container-fluid">
+        
+            <div className="row">
+            <div className="col-12 title">
+                <div className="img">
+                    <img src="LGM2.jpeg" alt="img" />
+                </div>
+                <h1 className="text-center"><cite>Calculator</cite></h1>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+}
+export default CalHead;
